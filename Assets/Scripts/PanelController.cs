70f95852b4ef07459ddeb6e7395dc602ec2926f5
matsubara daisuke <matsubara@jfrontier.net>
2020-08-03 17:47:01 +0900

    initial commit

---
+++ Assets/Scripts/PanelController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelController : MonoBehaviour
+{
+    [SerializeField]
+    private Button yesNoButton;
+
+    [SerializeField]
+    private Button yesButton;
+
+    [SerializeField]
+    private YesNoDialog yesNoDialog;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        yesButton.onClick.AddListener( callYesDialog );
+        yesNoButton.onClick.AddListener( callYesNoDialog );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void callYesDialog(){
+
+        yesNoDialog. Open("yesダイアログです", "Yes",  (isYes) => {
+
+                Debug.Log("yesが押されました");               
+
+        });
+
+    }
+
+    private void callYesNoDialog(){
+        yesNoDialog. Open("yesNoダイアログです", "Yes", "No", (isYes) => {
+
+            if(isYes){
+                Debug.Log("yesが押されました");              
+            }else{
+                Debug.Log("noが押されました");  
+            }
+        });
+    }
+    
+}
---
+++ Assets/Scripts/YesNoDialog.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class YesNoDialog : MonoBehaviour
+{
+
+    public enum ButtonType {
+        YesNo,
+        Yes,
+    };
+
+    struct DispData {
+        public string title;
+        public string body;
+        public string yesText;
+        public string noText;
+        public ButtonType btnType;
+
+        public DispData(string title, string body, ButtonType btnType) {
+            this.title = title;
+            this.body = body;
+            this.btnType = btnType;
+            this.yesText = "OK";
+            this.noText = "キャンセル";
+        }
+        public DispData(string title, string body, ButtonType btnType, string yesText, string noText) {
+            this.title = title;
+            this.body = body;
+            this.btnType = btnType;
+            this.yesText = yesText;
+            this.noText = noText;
+        }
+    };
+
+    public delegate void EndDelegate(bool isYes);
+    EndDelegate endDelegate;
+
+    public Text BodyText;
+    public Text YesText;
+    public Text NoText;
+    public Text OkText;
+
+    public GameObject YesPanel;
+    public GameObject YesNoPanel;
+    public GameObject BasePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //BasePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool IsOpen()
+    {
+        return gameObject.activeSelf;
+    }
+
+
+    // yesNoButtonの場合
+    public void Open(string bodyText, string yesText, string noText, EndDelegate endDel=null)
+    {
+        gameObject.SetActive( true );
+
+        BodyText.text = bodyText;
+        if( YesText ) {
+            YesText.text = yesText;
+        }
+        if( NoText ) {
+            NoText.text = noText;
+        }
+        /*
+        if( OkText ) {
+            OkText.text = dispData[dispType].yesText;
+        }
+        */
+        SetButtonType( ButtonType.YesNo );
+
+        if (endDel != null)
+        {
+            endDelegate = endDel;
+        } else {
+            endDelegate = (isYes) => { };
+        }
+        BasePanel.SetActive(true);
+
+    }
+
+    // yesButtonの場合
+    public void Open(string bodyText, string okText, EndDelegate endDel=null)
+    {
+        gameObject.SetActive( true );
+        Debug.Log("call open");
+
+        BodyText.text = bodyText;
+
+        if( OkText ) {
+            OkText.text = okText;
+        }
+
+        SetButtonType( ButtonType.Yes );
+
+        if (endDel != null)
+        {
+            endDelegate = endDel;
+        } else {
+            endDelegate = (isYes) => { };
+        }
+        BasePanel.SetActive(true);
+
+    }
+
+    public void Close()
+    {
+        BasePanel.SetActive(false);
+        gameObject.SetActive( false );
+    }
+
+    public void SetButtonType(ButtonType btnType) {
+        if (btnType == ButtonType.YesNo) {
+           YesPanel.SetActive( false );
+           YesNoPanel.SetActive( true );
+        }
+        else {
+           YesPanel.SetActive( true );
+           YesNoPanel.SetActive( false );
+        }
+    }
+
+    public void OnYesButton()
+    {
+        if (endDelegate != null)
+        {
+            endDelegate(true);
+        }
+        Close();
+    }
+
+    public void OnNoButton()
+    {
+        if (endDelegate != null)
+        {
+            endDelegate(false);
+        }
+        Close();
+    }
+
+}
